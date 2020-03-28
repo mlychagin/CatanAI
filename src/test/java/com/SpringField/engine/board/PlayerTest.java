bd5b630b24c137df27f3352ab22c0e9e79b4bbef
Marco3dog <marco.caruso13@gmail.com>
2020-03-27 17:38:12 -0700

    marco first commit

--- src/test/java/com/SpringField/engine/board/PlayerTest.java
+++ src/test/java/com/SpringField/engine/board/PlayerTest.java
@@ -1,4 +1,18 @@
 package com.SpringField.engine.board;
 
+import org.junit.Test;
+
+import static com.SpringField.engine.util.Util.BRICK;
+import static com.SpringField.engine.util.Util.WOOD;
+
 public class PlayerTest {
+    @Test
+    public void buyRoadTest(){
+        Player P = new Player();
+        P.addResource(WOOD, (byte) 1);
+        P.addResource(BRICK, (byte) 1);
+        assert P.canBuyRoad();
+        P.buyRoad();
+    }
+    
 }
