b460aee0c90edf585dc8da5e42da43525a736b8c
Bryan Carl <bryancarl7@gmail.com>
2020-03-29 19:19:54 -0700

    added stuff

--- src/test/java/com/SpringField/engine/ai/settleTest.java
+++ src/test/java/com/SpringField/engine/ai/settleTest.java
@@ -13,6 +13,59 @@ import static com.SpringField.engine.util.Util.*;
 import static com.SpringField.engine.util.Util.KNIGHT;
 
 public class settleTest {
+    public static void main(String[] args) throws IOException{
+        // Quick bulk testing to get us off the ground
+        // Quick I/O to link with a csv file
+        String pathToCsv ="";
+        FileWriter csvWriter;
+        try {
+            pathToCsv = "output.txt";
+            csvWriter = new FileWriter(pathToCsv);
+        } catch (IOException e){
+            throw new IOException("Could not locate: " + pathToCsv);
+        }
+
+        // Start the timer
+        long startTime = System.nanoTime();
+
+        // 1000 iterations of a board state being settled
+        int i = 0;
+        while (i < 100000) {
+            String settles = "";
+            BoardState test = new BoardState(4);
+            Settlement chooser = new Settlement(test);
+            for (byte n = 0; n < 8; n++) {
+                byte next_settle = chooser.getBestPossibleSettle();
+                settles = settles + next_settle + " ";
+                try {
+                    test.buildSettlement(next_settle, false);
+                } catch (RuntimeException e) {
+                    throw new RuntimeException("Could not build settlement");
+                }
+            }
+
+            // Reset the bnoard and forward the counters
+            initializeTiles();
+            i++;
+
+            // loop to print out pertinent data
+            String[] arr = settles.split(" ");
+            for (int j = 0; j < 7;j++) {
+                //if ((j + 1) == 7) {
+                csvWriter.write(arr[j]);
+                //}
+            }
+            csvWriter.flush();
+        }
+
+        csvWriter.close();
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("\nExecuted and settled " + i + " boardstates in: " + (duration*(1.0)/1000000000) + " second(s)!");
+    }
+
+
     @Test
     public void canInitBoardstate() {
         BoardState test = new BoardState(4);
@@ -60,61 +113,6 @@ public class settleTest {
         // playerWithLongestRoad
         // currentLongestRoad
         // playerTurn
-    }
-
-    @Test
-    public void bulkTesting() throws IOException {
-        // Quick I/O to link with a csv file
-        String pathToCsv ="";
-        FileWriter csvWriter;
-        try {
-            pathToCsv = "tracker.csv";
-            csvWriter = new FileWriter(pathToCsv);
-        } catch (IOException e){
-            throw new IOException("Could not locate: " + pathToCsv);
-        }
-
-        // Start the timer
-        long startTime = System.nanoTime();
-
-        // 1000 iterations of a board state being settled
-        int i = 0;
-        while (i < 100000) {
-            String settles = "";
-            BoardState test = new BoardState(4);
-            Settlement chooser = new Settlement(test);
-            for (byte n = 0; n < 8; n++) {
-                byte next_settle = chooser.getBestPossibleSettle();
-                settles = settles + next_settle + " ";
-                try {
-                    test.buildSettlement(next_settle, false);
-                } catch (RuntimeException e) {
-                    throw new RuntimeException("Could not build settlement");
-                }
-            }
-
-            // Reset the bnoard and forward the counters
-            initializeTiles();
-            i++;
-
-            // loop to print out pertinent data
-            String[] arr = settles.split(" ");
-            for (byte j = 0; j < 7;j++){
-                if ((j + 1) == 7){
-                    csvWriter.write(arr[j] + "\n");
-                    break;
-                }
-                csvWriter.write(arr[j] + ",");
-            }
-
-        }
-
-        csvWriter.flush();
-        csvWriter.close();
-
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        System.out.println("\nExecuted and settled " + i + " boardstates in: " + (duration*(1.0)/1000000000) + " second(s)!");
     }
 
 }
