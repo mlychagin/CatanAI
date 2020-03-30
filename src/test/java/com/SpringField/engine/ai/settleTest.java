e8c054aa1f7ebf391cd1edefb7ba1140201e7d2e
Bryan Carl <bryancarl7@gmail.com>
2020-03-29 18:44:28 -0700

    testing

--- src/test/java/com/SpringField/engine/ai/settleTest.java
+++ src/test/java/com/SpringField/engine/ai/settleTest.java
@@ -1,10 +1,12 @@
 package com.SpringField.engine.ai;
 
+import com.SpringField.ai.Settlement;
 import com.SpringField.engine.board.Player;
 import com.SpringField.engine.BoardState;
 import com.SpringField.engine.board.Vertex;
 import org.junit.Test;
 
+import java.io.*;
 import java.util.Random;
 
 import static com.SpringField.engine.util.Util.*;
@@ -33,6 +35,24 @@ public class settleTest {
             System.out.println(p);
         }
 
+        Settlement chooser = new Settlement(test);
+
+        for (byte i = 0; i<8;i++) {
+            byte next_settle = chooser.getBestPossibleSettle();
+            try {
+                test.buildSettlement(next_settle, false);
+            }
+            catch (RuntimeException e){
+                throw new RuntimeException("could not build settlement");
+            }
+        }
+
+        // vertices
+        Vertex[] after = test.getVertices();
+        for (Vertex v : after) {
+            System.out.println(v);
+        }
+
         // TODO:
         // resourceCardPool
         // devCardPool
@@ -40,6 +60,61 @@ public class settleTest {
         // playerWithLongestRoad
         // currentLongestRoad
         // playerTurn
+    }
+
+    @Test
+    public void bulkTesting() throws IOException {
+        // Quick I/O to link with a csv file
+        String pathToCsv ="";
+        FileWriter csvWriter;
+        try {
+            pathToCsv = "tracker.csv";
+            csvWriter = new FileWriter(pathToCsv);
+        } catch (IOException e){
+            throw new IOException("Could not locate: " + pathToCsv);
+        }
+
+        // Start the timer
+        long startTime = System.nanoTime();
+
+        // 1000 iterations of a board state being settled
+        int i = 0;
+        while (i < 100000) {
+            String settles = "";
+            BoardState test = new BoardState(4);
+            Settlement chooser = new Settlement(test);
+            for (byte n = 0; n < 8; n++) {
+                byte next_settle = chooser.getBestPossibleSettle();
+                settles = settles + next_settle + " ";
+                try {
+                    test.buildSettlement(next_settle, false);
+                } catch (RuntimeException e) {
+                    throw new RuntimeException("Could not build settlement");
+                }
+            }
+
+            // Reset the bnoard and forward the counters
+            initializeTiles();
+            i++;
+
+            // loop to print out pertinent data
+            String[] arr = settles.split(" ");
+            for (byte j = 0; j < 7;j++){
+                if ((j + 1) == 7){
+                    csvWriter.write(arr[j] + "\n");
+                    break;
+                }
+                csvWriter.write(arr[j] + ",");
+            }
+
+        }
+
+        csvWriter.flush();
+        csvWriter.close();
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("\nExecuted and settled " + i + " boardstates in: " + (duration*(1.0)/1000000000) + " second(s)!");
     }
 
 }
